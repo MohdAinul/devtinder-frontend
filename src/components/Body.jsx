f96614b57760099d88430d1552cf8b49a9338658
MohdAinul <mohdainul01@gmail.com>
2026-02-12 22:27:31 +0530

    add Connections + Request

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from './NavBar'
-import Footer from './Footer'
+import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./NavBar";
+import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
-    return (
-      <div>
-        <NavBar />
-        <Outlet />
-        <Footer />
-      </div>
-    );
-}
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
-export default Body
+  const fetchUser = async () => {
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      // console.log(user.data);
+      dispatch(addUser(user.data));
+    } catch (err) {
+      if (err.status == 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      fetchUser();
+    }
+  }, []);
+  return (
+    <div>
+      <Navbar />
+      <div className="pt-20">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
